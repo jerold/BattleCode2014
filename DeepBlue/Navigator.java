e8490ac12fd596a7c606a778c7caef52774f5c32
Jerold <AfterHours@WF10506.local>
2014-01-31 23:26:34 -0700

    safe tileType method now checks out of bounds locations

--- DeepBlue/Navigator.java
+++ DeepBlue/Navigator.java
@@ -66,7 +66,7 @@ public class Navigator {
             defaultMovement();
         else if (map.pathingStrat == RoadMap.PathingStrategy.SmartBug)
             smartMovement();
-        rc.setIndicatorString(1, "["+map.pathingStrat+"]  Dest"+destination+"  Term"+dogBugTerminal+"  Dog"+dog+"  Sit["+dogSitting+"]  DD["+(int)Utilities.distanceBetweenTwoPoints(me, destination)+"] EQ["+(int)Utilities.distanceBetweenTwoPoints(me, cache.ENEMY_HQ)+"]");
+//        rc.setIndicatorString(1, "["+map.pathingStrat+"]  Dest"+destination+"  Term"+dogBugTerminal+"  Dog"+dog+"  Sit["+dogSitting+"]  DD["+(int)Utilities.distanceBetweenTwoPoints(me, destination)+"] EQ["+(int)Utilities.distanceBetweenTwoPoints(me, cache.ENEMY_HQ)+"]");
     }
 
     public void setSneak(boolean setting)
