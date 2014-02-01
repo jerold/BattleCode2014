24459798ad6a8cf89c44bb3a5a162bc759c024bc
Jerold <AfterHours@WF10506.local>
2014-01-31 17:36:16 -0700

    strat update

--- DeepBlue/UnitStrategy.java
+++ DeepBlue/UnitStrategy.java
@@ -22,20 +22,10 @@ public abstract class UnitStrategy {
         if (Clock.getRoundNum()-lastRallyRecheck > rallyRecheckInterval) {
             MapLocation rallyPointOnWire = VectorFunctions.intToLoc(Soldiers.rc.readBroadcast(Utilities.startRallyPointChannels+rallyChannelMod*2));
             if (rallyPointOnWire.x != rallyPoint.x && rallyPointOnWire.y != rallyPoint.y) {
-                boolean hadArrived = Soldiers.nav.hasArrived;
                 Soldiers.nav.setDestination(rallyPointOnWire);
-                setNextStep(hadArrived, rallyPoint, rallyPointOnWire);
                 rallyPoint = rallyPointOnWire;
             }
             lastRallyRecheck = Clock.getRoundNum();
-        }
-    }
-
-    public static void setNextStep(boolean hadArrived, MapLocation oldRally, MapLocation newRally) throws GameActionException
-    {
-        if (Soldiers.map.mapUploaded) {
-            Soldiers.nav.nextStepNodeId = Soldiers.map.idForNodeNearOldRallyPointInDirectionOfNewRallyPoint(oldRally, newRally);
-            Soldiers.nav.nextStep = Soldiers.map.locationForNode(Soldiers.nav.nextStepNodeId);
         }
     }
 }
